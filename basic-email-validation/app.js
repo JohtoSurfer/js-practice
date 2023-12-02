087c054ee306bfb64b2237f967955ba44ec21c7f
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2023-12-02 12:48:45 -0400

    type: bug fixed summary: my first vertion of the validator did not work, app.js file was updated event listener properly implemented note: im still learning and having fun with the basic feel free to leave a note.

--- basic-email-validation/app.js
+++ basic-email-validation/app.js
@@ -1,13 +1,13 @@
 function valid() {
     let email = document.getElementById("text").value;
-    let regex = /^([a-zA-Z0-9\.]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+    let regex = /^([a-zA-Z0-9\.]+)@([a-zA-Z0-9])+\.([a-z]+)(\.[a-z]+)?$/;
 
-    if(regex.text(email)) {
-        alert("Valid Access")
-        return true
+    if(regex.test(email)) {
+        alert("Valid Access");
+        return true;
     } else {
-        alert("Access Denied")
+        alert("Access Denied");
     }
 }
 
-addEventListener(valid);
+document.getElementById("btn").addEventListener("click", valid);
