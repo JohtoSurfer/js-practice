cc58b5d130b929003178d764bdb8779967ac900d
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2024-03-07 10:26:59 -0400

    type: fix and update. summary: name file fixed. functions added. note: still testing and debugging features.

--- rpg-basic/app.js
+++ rpg-basic/app.js
@@ -12,10 +12,10 @@ const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#silverText");
+const silverText = document.querySelector("#silverText");
 const monsterStats = document.querySelector("#demonStats");
-const monsterName = document.querySelector("#demonName");
-const monsterHealthText = document.querySelector("#demonHealth");
+const demonName = document.querySelector("#demonName");
+const demonrHealthText = document.querySelector("#demonHealth");
 const weapons = [
   { name: 'holy water', power: 5 },
   { name: 'silver dagger', power: 30 },
@@ -24,29 +24,82 @@ const weapons = [
 ];
 const locations = [
   {
-    name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goCurseTown, goHauntedForest, fightDemon],
+    name: "cursed Town",
+    "button text": ["Go to store", "Go to haunted forest", "Fight Demon"],
+    "button functions": [goShop, goHauntedForest, fightDemon],
     text: "You are in the town square. You see a sign that says \"Store\"."
   },
   {
     name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon, goCurseTown],
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 silver)", "Go to cursed town square"],
+    "button functions": [buyHealth, buyWeapon, goShop],
     text: "You enter the store."
   },
   {
-    name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightGhost, fightUndead, goCurseTown],
-    text: "You enter the cave. You see some monsters."
+    name: "haunted forest",
+    "button text": ["Fight maid ghost", "Fight undead warrior", "Go to town square"],
+    "button functions": [fightGhost, fightUndead, goShop],
+    text: "You enter the haunted forest. You see some evil spectres."
   }
 ];
 
 // init btn 
-button1.onclick = goCurseTown;
+button1.onclick = goShop;
 button2.onclick = goHauntedForest;
 button3.onclick = fightDemon;
 
 // functionality 
+
+function update(location) {
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
+  }
+  
+  function goCursedTown() {
+    update(locations[0]);
+  }
+  
+  function goShop() {
+    update(locations[1]);
+  }
+  
+  function goHauntedForest() {
+    update(locations[2]);
+  }
+  
+  function fightDemon() {
+    console.log("Fighting demon.");
+  }
+  
+  function buyHealth() {
+    if (silver >= 10) {
+      silver -= 10;
+      silver += 10;
+      silverText.innerText = silver;
+      silverText.innerText = health;
+    } else {
+      text.innerText = "You do not have enough silver to buy health.";
+    }
+  }
+
+  function buyWeapon() {
+    if (currentWeapon < 3) {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+      } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
+      }
+    }
+  }
 
