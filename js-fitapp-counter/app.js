a435f9e3a38b837acc9f374ebdd725a1aa464fd4
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2024-03-26 21:37:37 -0400

    type: update. summary:figuring out how to add entry using regex

--- js-fitapp-counter/app.js
+++ js-fitapp-counter/app.js
@@ -13,3 +13,13 @@ function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, '');
   }
+
+  function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+  }
+
+  function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    
+  }
