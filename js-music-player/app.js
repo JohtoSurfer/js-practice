a82198701281d5872857e084f9bc5d66126ea39f
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2024-04-16 15:33:44 -0400

    type: update summary: function that sort songs created. note: testing the new funct

--- js-music-player/app.js
+++ js-music-player/app.js
@@ -107,3 +107,21 @@ const allSongs = [
   
     playlistSongs.innerHTML = songsHTML;
   };
+
+  const sortSongs = () => {
+    userData?.songs.sort((a,b) => {
+      if (a.title < b.title) {
+        return -1;
+      }
+  
+      if (a.title > b.title) {
+        return 1;
+      }
+  
+      return 0;
+    });
+  
+    return userData?.songs;
+  };
+  
+  renderSongs(sortSongs());
